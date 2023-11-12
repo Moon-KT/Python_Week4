36c6f7259c6dcfd6808fe07a5b4664f174d1c12b
Moon-KT <136817363+Moon-KT@users.noreply.github.com>
2023-11-12 23:51:54 +0700

    fix: fixed Bai 3.py

--- Bai 3.py
+++ Bai 3.py
@@ -7,3 +7,10 @@ list = re.findall(r'-?\d+', str)
 
 sum = sum(int(i) for i in list )
 print("Sum of list: ", sum)
+
+
+# Note trên slide Buổi 4:
+# findall(pattern, str): return 1 list chứa tất cả các matches
+#  \ special charecter  Vd re.findall(“\d”, txt)
+#  ? xuất hiện 0 hoặc 1 Vdre.findall(“.?t”, txt)
+
